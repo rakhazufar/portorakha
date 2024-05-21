4671f7e53eb7e8748e87fdf9001a8f8f2768ce2d
rakhazufar <rakhazufar1@gmail.com>
2024-05-22 00:46:22 +0700

    responsive plane header

--- src/app/components/pesawat.jsx
+++ src/app/components/pesawat.jsx
@@ -1,14 +1,7 @@
 function Plane() {
   return (
-    <div>
-      <iframe
-        src="pesawat/index.html"
-        width="100%"
-        style={{
-          height: "30vh",
-          border: "none",
-        }}
-      >
+    <div class="responsive-iframe">
+      <iframe src="pesawat/index.html" width="100%" style={{ border: "none" }}>
         Your browser does not support iframes.
       </iframe>
     </div>
